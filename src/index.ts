89bd3803c4df12916b524f76fb23e12642e60fc4
Xaho <mauriceampt@hotmail.com>
2026-04-18 14:37:17 +0200

    Refactoring: Extract functions, add filters for routes and markers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,36 @@
 /// <reference types="@types/jquery" />
 /// <reference types="@types/jqueryui" />
 
+enum MARKER_TYPE {
+    HOTEL = 'Hotel',
+    POI = 'Point of interest',
+    EVENT = 'Event',
+    ROUTE_START = 'Route start'
+}
+
+enum LINE_TYPE {
+    CAR = 'Car',
+    TRAIN = 'Train',
+    BUS = 'Bus',
+    FERRY = 'Ferry'
+}
+
+type MarkerType = 'hotel' | 'POI' | 'event' | 'RouteStart';
+type LineType = 'car' | 'train' | 'bus' | 'ferry';
+
+let AdvancedMarkerElement: typeof google.maps.marker.AdvancedMarkerElement;
+let PinElement: typeof google.maps.marker.PinElement;
+
+const MarkerFilters: HTMLInputElement[] = [];
+const LineFilters: HTMLInputElement[] = [];
+let minYear = 2016;
+let maxYear = 2026;
+
+const elements: {
+    markers: { element: HTMLElement, date: Date, type: MARKER_TYPE }[],
+    lines: { element: google.maps.Polyline, date: Date, type: LINE_TYPE }[]
+} = {markers: [], lines: []};
+
 async function parseTour(url: string) {
     const routeXml = await (await fetch(url)).text();
     const parser = new DOMParser();
@@ -11,28 +41,171 @@ async function parseTour(url: string) {
     });
 }
 
-async function initMap() {
-    const {AdvancedMarkerElement, PinElement} = await google.maps.importLibrary('marker');
+function updateVisibility() {
+    for (const marker of elements.markers) {
+        let visible = true;
+        if (marker.date.getFullYear() < minYear || marker.date.getFullYear() > maxYear) visible = false;
+        else if (MarkerFilters.some(f => f.name === marker.type && !f.checked)) visible = false;
+        marker.element.style.visibility = visible ? 'visible' : 'hidden';
+    }
+    for (const line of elements.lines) {
+        let visible = true;
+        if (line.date.getFullYear() < minYear || line.date.getFullYear() > maxYear) visible = false;
+        else if (LineFilters.some(f => f.name === line.type && !f.checked)) visible = false;
+        line.element.setVisible(visible);
+    }
+}
+
+function generateCheckboxesForMarkers(target: HTMLElement) {
+    const container = document.createElement('div');
+    const header = document.createElement('h1');
+    header.textContent = 'Markers';
+    container.append(header);
+    for (let markertypeKey of Object.values(MARKER_TYPE)) {
+        const row = document.createElement('div');
+        const checkbox = Object.assign(document.createElement('input'), {
+            type: 'checkbox',
+            checked: true,
+            name: markertypeKey,
+            value: markertypeKey
+        });
+        MarkerFilters.push(checkbox);
+        checkbox.addEventListener('change', (e) => {
+            updateVisibility();
+        });
+        row.append(checkbox, markertypeKey);
+        container.append(row);
+    }
+    target.append(container);
+}
+
+function generateCheckboxesForLines(target: HTMLElement) {
+    const container = document.createElement('div');
+    const header = document.createElement('h1');
+    header.textContent = 'Routes';
+    container.append(header);
+    for (let lineTypeKey of Object.values(LINE_TYPE)) {
+        const row = document.createElement('div');
+        const checkbox = Object.assign(document.createElement('input'), {
+            type: 'checkbox',
+            checked: true,
+            name: lineTypeKey,
+            value: lineTypeKey
+        });
+        LineFilters.push(checkbox);
+        checkbox.addEventListener('change', (e) => updateVisibility());
+        row.append(checkbox, lineTypeKey);
+        container.append(row);
+    }
+    target.append(container);
+}
+
+function createButton() {
+    const controlButton = Object.assign(
+        document.createElement('button'),
+        {
+            textContent: 'Center Map',
+            title: 'Click to recenter the map',
+        });
+    controlButton.addEventListener('click', () => {});
+    return controlButton;
+}
+
+async function addEventToMap(map: google.maps.Map, {title, date, albumUrl, position, type}: {
+    title: string,
+    date: Date,
+    albumUrl: string,
+    position: { lat: number, lng: number },
+    type: MARKER_TYPE
+}) {
+    const marker = new AdvancedMarkerElement({
+        map,
+        position,
+        title,
+        content: createEventPin(`'${date.getFullYear() - 2000}`)
+    });
+
+    if (albumUrl) {
+        marker.addListener('gmp-click', () => window.open(albumUrl, '_blank'));
+    }
+    return {element: marker, date, type};
+}
+
+function createRoutePin(text: string, pitstop = false) {
+    return new PinElement({
+        glyphText: pitstop ? undefined : text,
+        glyphColor: pitstop ? undefined : 'white',
+    });
+}
+
+function createEventPin(text: string) {
+    return new PinElement({
+        glyphText: text,
+        background: '#1965C4',
+        glyphColor: 'white',
+        borderColor: '#1965C4'
+    });
+}
+
+async function loadGpxToGmaps(map: google.maps.Map, {files, albumUrl, date, type = LINE_TYPE.CAR, color = '#FF0000'}: {
+    files: string[],
+    albumUrl: string,
+    date: Date,
+    type: LINE_TYPE,
+    color?: string
+}) {
     const elements: {
-        markers: { element: HTMLElement, date: Date, type: string }[],
-        lines: { element: google.maps.Polyline, date: Date, type: string }[]
+        markers: { element: HTMLElement, date: Date, type: MARKER_TYPE }[],
+        lines: { element: google.maps.Polyline, date: Date, type: LINE_TYPE }[]
     } = {markers: [], lines: []};
-
-    function createRoutePin(text: string, pitstop = false) {
-        return new PinElement({
-            glyphText: pitstop ? undefined : text,
-            glyphColor: pitstop ? undefined : 'white',
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const path = await parseTour(file);
+        const polyline = new google.maps.Polyline({
+            path,
+            geodesic: true,
+            strokeColor: color,
+            strokeOpacity: .6,
+            strokeWeight: 3,
         });
-    }
-
-    function createEventPin(text: string) {
-        return new PinElement({
-            glyphText: text,
-            background: '#1965C4',
-            glyphColor: 'white',
-            borderColor: '#1965C4'
+        elements.lines.push({element: polyline, date, type});
+        polyline.setMap(map);
+        polyline.set('originalColor', color);
+        google.maps.event.addListener(polyline, 'mouseover', () => {
+            polyline.setOptions({strokeColor: '#1493FF', zIndex: 1, strokeOpacity: 1});
         });
+        google.maps.event.addListener(polyline, 'mouseout', () => {
+            polyline.setOptions({strokeColor: polyline.get('originalColor'), zIndex: 0, strokeOpacity: .6});
+        });
+        const markerContent = createRoutePin(`'${date.getFullYear() - 2000}`, i > 0);
+        markerContent.onmouseenter = function (event) {
+            polyline.setOptions({strokeColor: '#1493FF', zIndex: 1, strokeOpacity: 1});
+            event.stopPropagation();
+            event.preventDefault();
+        };
+        markerContent.onmouseleave = function (event) {
+            polyline.setOptions({strokeColor: polyline.get('originalColor'), zIndex: 0, strokeOpacity: .6});
+            event.stopPropagation();
+            event.preventDefault();
+        };
+        const marker = new AdvancedMarkerElement({
+            map,
+            position: path[0],
+            title: /..\/resources\/(?<tourname>.*)\.gpx/.exec(file)?.groups?.tourname,
+            content: markerContent,
+            gmpClickable: !!albumUrl,
+            zIndex: 1 - i
+        });
+        if (albumUrl) {
+            marker.addListener('gmp-click', () => window.open(albumUrl, '_blank'));
+        }
+        elements.markers.push({element: marker, date, type: MARKER_TYPE.ROUTE_START});
     }
+    return elements;
+}
+
+async function initMap() {
+    ({AdvancedMarkerElement, PinElement} = await google.maps.importLibrary('marker'));
 
     const map = new google.maps.Map(document.getElementById('map')!, {
         zoom: 8,
@@ -52,83 +225,6 @@ async function initMap() {
     legend.append(yearPin, 'Tour in the year \'XX\'', pitstopPin, 'Planned stop in route', eventPin, 'Event in the year \'XX\'', note);
 
     map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
-
-    async function loadGpxToGmaps({files, albumUrl, date, type = '', color = '#FF0000'}: {
-        files: string[],
-        albumUrl: string,
-        date: Date,
-        type: string,
-        color?: string
-    }) {
-        const elements: {
-            markers: { element: HTMLElement, date: Date, type: string }[],
-            lines: { element: google.maps.Polyline, date: Date, type: string }[]
-        } = {markers: [], lines: []};
-        for (let i = 0; i < files.length; i++) {
-            const file = files[i];
-            const path = await parseTour(file);
-            const polyline = new google.maps.Polyline({
-                path,
-                geodesic: true,
-                strokeColor: color,
-                strokeOpacity: .6,
-                strokeWeight: 3,
-            });
-            elements.lines.push({element: polyline, date, type});
-            polyline.setMap(map);
-            polyline.set('originalColor', color);
-            google.maps.event.addListener(polyline, 'mouseover', () => {
-                polyline.setOptions({strokeColor: '#1493FF', zIndex: 1, strokeOpacity: 1});
-            });
-            google.maps.event.addListener(polyline, 'mouseout', () => {
-                polyline.setOptions({strokeColor: polyline.get('originalColor'), zIndex: 0, strokeOpacity: .6});
-            });
-            const markerContent = createRoutePin(`'${date.getFullYear() - 2000}`, i > 0);
-            markerContent.onmouseenter = function (event) {
-                polyline.setOptions({strokeColor: '#1493FF', zIndex: 1, strokeOpacity: 1});
-                event.stopPropagation();
-                event.preventDefault();
-            };
-            markerContent.onmouseleave = function (event) {
-                polyline.setOptions({strokeColor: polyline.get('originalColor'), zIndex: 0, strokeOpacity: .6});
-                event.stopPropagation();
-                event.preventDefault();
-            };
-            const marker = new AdvancedMarkerElement({
-                map,
-                position: path[0],
-                title: /..\/resources\/(?<tourname>.*)\.gpx/.exec(file)?.groups?.tourname,
-                content: markerContent,
-                gmpClickable: !!albumUrl,
-                zIndex: 1 - i
-            });
-            if (albumUrl) {
-                marker.addListener('gmp-click', () => window.open(albumUrl, '_blank'));
-            }
-            elements.markers.push({element: marker, date, type});
-        }
-        return elements;
-    }
-
-    async function addEventToMap({title, date, albumUrl, position, type}: {
-        title: string,
-        date: Date,
-        albumUrl: string,
-        position: { lat: number, lng: number },
-        type: string
-    }) {
-        const marker = new AdvancedMarkerElement({
-            map,
-            position,
-            title,
-            content: createEventPin(`'${date.getFullYear() - 2000}`)
-        });
-
-        if (albumUrl) {
-            marker.addListener('gmp-click', () => window.open(albumUrl, '_blank'));
-        }
-        return {element: marker, date, type};
-    }
 
     for (const event of [
         {
@@ -198,7 +294,7 @@ async function initMap() {
             position: {lat: 51.714717, lng: 4.883165 + 0.002}
         }
     ]) {
-        elements.markers.push(await addEventToMap({...event, type: 'event'}));
+        elements.markers.push(await addEventToMap(map, {...event, type: MARKER_TYPE.EVENT}));
     }
     for (const route of [
         {
@@ -297,58 +393,41 @@ async function initMap() {
             albumUrl: 'https://photos.app.goo.gl/ELhtDxuBMDEeNP5y5',
             date: new Date('2025-10-19'),
         }]) {
-        const {markers, lines} = await loadGpxToGmaps({...route, type: 'car'});
+        const {markers, lines} = await loadGpxToGmaps(map, {...route, type: LINE_TYPE.CAR});
         elements.markers.push(...markers);
         elements.lines.push(...lines);
     }
 
-    function createButton() {
-        const controlButton = Object.assign(
-            document.createElement('button'),
-            {
-                textContent: 'Center Map',
-                title: 'Click to recenter the map',
-            });
-        controlButton.addEventListener('click', () => {
-        });
-        return controlButton;
-    }
-
-    const button = createButton();
-    const filter = document.createElement('div');
-    filter.id = 'filters';
-    const input = document.createElement('p');
-    input.id = 'amount';
+    const filterDiv = document.createElement('div');
+    filterDiv.id = 'filters';
+    const yearRangeParagraph = document.createElement('p');
+    yearRangeParagraph.id = 'amount';
+    const yearHeader = document.createElement('h1');
+    yearHeader.textContent = 'Year';
     const slider = document.createElement('div');
     slider.id = 'slider-range';
-    filter.append(button, input, slider);
-    map.controls[google.maps.ControlPosition.LEFT_CENTER].push(filter);
+
+    filterDiv.append(yearHeader, yearRangeParagraph, slider);
+    generateCheckboxesForLines(filterDiv);
+    generateCheckboxesForMarkers(filterDiv);
+
+    map.controls[google.maps.ControlPosition.LEFT_CENTER].push(filterDiv);
 
     function updateAmount(lower: number, upper: number) {
-        $(input).text(`${lower} - ${upper}`);
-        // set visibility
-        elements.markers.forEach(marker => {
-            marker.element.style.visibility = marker.date.getFullYear() <= upper && marker.date.getFullYear() >= lower ?
-                'visible' : 'hidden';
-        });
-        elements.lines.forEach(line => {
-            line.element.setVisible(
-                (line.date.getFullYear() <= upper && line.date.getFullYear() >= lower)
-            );
-        });
+        $(yearRangeParagraph).text(`${lower} - ${upper}`);
+        minYear = lower;
+        maxYear = upper;
+        updateVisibility();
     }
 
     $(slider).slider({
         range: true,
-        min: 2016,
-        max: new Date().getFullYear(),
-        values: [new Date().getFullYear(), new Date().getFullYear()],
+        min: minYear,
+        max: maxYear,
+        values: [minYear, maxYear],
         slide: function (event, ui) {
             updateAmount(ui.values?.[0] ?? 0, ui.values?.[1] ?? 0);
         }
     });
-    updateAmount(new Date().getFullYear(), new Date().getFullYear());
-
-    // Type filters
-    // Determine options based on entries in elements array
+    updateAmount(minYear, maxYear);
 }
